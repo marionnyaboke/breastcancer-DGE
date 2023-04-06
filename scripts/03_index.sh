84c72d37f4d3b10e4b5c3f0f1670f5e16f0a5371
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2023-04-06 11:05:10 +0300

    Update 03_index.sh

--- scripts/03_index.sh
+++ scripts/03_index.sh
@@ -7,9 +7,9 @@
 # load the blast module
 module load hisat2
 
-cd /home/emurungi/gitau/marion/raw
+cd /home/gitau/marion/raw
 
-FNA_DIR="/home/emurungi/gitau/marion/raw"
+FNA_DIR="/home/gitau/marion/raw"
 
 #Index reference using HISAT2
 hisat2-build ${FNA_DIR}/GCF_000001405.39_GRCh38.p13_genomic.fna GCF_000001405.39_GRCh38.p13_genomic.fna_index_hisat2
