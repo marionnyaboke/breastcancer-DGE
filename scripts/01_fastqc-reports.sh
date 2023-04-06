6709f93703db32077f4f336be072eba397ab254b
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2023-04-06 11:02:49 +0300

    Update 01_fastqc-reports.sh

--- scripts/01_fastqc-reports.sh
+++ scripts/01_fastqc-reports.sh
@@ -6,15 +6,15 @@
 
 module load fastqc
 
-#mkdir -p /home/emurungi/gitau/marion/results/TNBCfastqc
+#mkdir -p /home/gitau/marion/results/TNBCfastqc
 
 #Script to generate FastQC reports using the FastQC tool
 
 # fastq files directory
-FASTQ_DIR="/home/emurungi/gitau/marion/TNBC/TNBCtrimmed"
+FASTQ_DIR="/home/gitau/marion/TNBC/TNBCtrimmed"
 
 # fastqc reports directory
-REPORT_DIR="/home/emurungi/gitau/marion/TNBC/fastqc-report"
+REPORT_DIR="/home/gitau/marion/TNBC/fastqc-report"
 
 for file in $FASTQ_DIR/*.fastq; do
    fastqc ${file} -o ${REPORT_DIR}
