aaedcedcfc16cb4a07ff1a2e47134b2eb74694bd
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2023-04-06 11:04:41 +0300

    Update 02_remove-adaptors.sh

--- scripts/02_remove-adaptors.sh
+++ scripts/02_remove-adaptors.sh
@@ -9,12 +9,12 @@ module load cutadapt
 #Script to trim adaptors
 
 
-mkdir -p /home/emurungi/gitau/marion/TNBC/TNBCtrimmed
+mkdir -p /home/gitau/marion/TNBC/TNBCtrimmed
 
 
-FASTQ_DIR="/home/emurungi/gitau/marion/TNBC/raw"
+FASTQ_DIR="/home/gitau/marion/TNBC/raw"
 
-OUT_DIR="/home/emurungi/gitau/marion/TNBC/TNBCtrimmed"
+OUT_DIR="/home/gitau/marion/TNBC/TNBCtrimmed"
 
 
 SAMPLES="SRR10729843 SRR10729844 SRR10729846 SRR10729847 SRR10729848 SRR10729849 SRR10729850 SRR10729851 SRR10729852 SRR10729853 SRR10729854 SRR10729855 SRR10729856 SRR10729857 SRR10729858"
