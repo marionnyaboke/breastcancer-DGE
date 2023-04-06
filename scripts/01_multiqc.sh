5eaaadbfcf0c340f47652f88e3163a988ce78bb5
marionnyaboke <45264074+marionnyaboke@users.noreply.github.com>
2023-04-06 11:04:08 +0300

    Update 01_multiqc.sh

--- scripts/01_multiqc.sh
+++ scripts/01_multiqc.sh
@@ -8,13 +8,13 @@ module load multiqc
 
 #Script to generate MultiQC reports using the MultiQC tool
 
-mkdir -p /home/emurungi/gitau/marion/results/multiqc
+mkdir -p /home/gitau/marion/results/multiqc
 
 # fastqc directory
-FASTQC_DIR="/home/emurungi/gitau/marion/results/fastqc1"
+FASTQC_DIR="/home/gitau/marion/results/fastqc1"
 
 # Multiqc output directory
-OUT_DIR="/home/emurungi/gitau/marion/results/multiqc"
+OUT_DIR="/home/gitau/marion/results/multiqc"
 
 # run mutiqc
 multiqc ${FASTQC_DIR}/*_fastqc.zip --outdir ${OUT_DIR}
